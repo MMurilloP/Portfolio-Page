a63f20a7027b7f674c5fddf729af1d828969a2fc
Manuel Murillo <mmpeira@gmail.com>
2023-05-04 16:22:39 +0200

    estructura del proyecto vite

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Contact from "./componets/Contact"
+import Hero from "./componets/Hero"
+import Who from "./componets/Who"
+import Works from "./componets/Works"
+
+import './App.css'
+
+
+
+
+function App() {
+
+  return (
+    <div className="app-container">
+      <h1>Hola</h1>
+      <Hero/>
+      <Who/>
+      <Works/>
+      <Contact/>
+    </div>
+  )
+}
+
+export default App
